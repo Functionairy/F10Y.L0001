d6e9fe40e886e35a6532eab1ed5b355361da9e96
davidcoats <david@getsafetycone.com>
2025-03-21 11:41:04 -0700

    Initial commit.

---
+++ source/F10Y.L0001.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace F10Y.L0001.Construction
+{
+	/// <summary>
+	/// Construction console project for the F10Y.L0001 library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/F10Y.L0001/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace F10Y.L0001
+{
+	/// <summary>
+	/// .NET Standard 2.1 foundation library for Functionairy.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
